3d611d51d4109a9e1fb60102bb2db18447c3442a
Yaroslav Georgitsa <yg@think.studio>
2020-12-15 09:46:56 +0200

    initial

---
+++ readme.md
@@ -0,0 +1,1 @@
+https://www.theregister.com/2018/02/12/icann_corp_home_mail_gtlds/
---
+++ scripts.js
@@ -0,0 +1,4 @@
+var div=document.createElement("div"); 
+div.classList.add('gcsc-ext-loc-banner');
+document.body.appendChild(div); 
+div.innerText="LOCAL";
